f47ac2a8b1462f9393419b10e335a16427e7ac48
Isabel Maito <maitoisabel@gmail.com>
2025-03-18 21:08:43 -0300

    add: correções

--- app.js
+++ app.js
@@ -4,17 +4,49 @@ const express = require('express');
 //Criação da aplicação 
 const app = express();
 
-//Define método Http Get que responde no path /
-app.get("/", (req, res) => {
+//Array de produtos
+const products =[
+    {
+        id: 1,
+        name: "Feijão",
+        brand: "Broto Legal",
+        barCode: "98327279823749327492732",
+        supplier: "Rede de Distribuição Ltda",
+        stockId: 98,
+        price: 8.79,
+        weight: 1,
+        measureUnit: "Kg"
+    },
+    {
+        id: 2,
+        name: "Arroz",
+        brand: "Tio João",
+        barCode: "7538745983750290298430923",
+        supplier: "Rede de Distribuição Ltda",
+        stockId: 65,
+        price: 2.99,
+        weight: 5,
+        measureUnit:"Kg"    
+    },
+]
+
+//Define método Http Get que responde no path /product/:id
+app.get("/product/:id", (req, res) => {
+    console.log(req.params.id);
+    
+    const product = products.find((product) => {
+        return products.id = Number(req.params.id)
+    })
+
     //Responde requisição com "Hello World!"
-    res.send("Hello World!");
+    res.status(200).json(product);
 });
 
-//Define método Http Post que responde no path /
-app.post("/", (req, res) => {
-    //Responde requisição com "Hello World Again"
-    res.send("Hello World Again");
+//Rota que retorna uma lista de produtos no path /product/:id
+app.get("/product", (req, res) => {
+    res.status(200).json(products);
 });
+
 
 //Inicia aplicação na porta 3000
 app.listen(3000, () => {
