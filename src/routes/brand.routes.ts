7b6038d51cf17cd3f376fbdf80d82069125d4eea
Isabel Maito <maitoisabel@gmail.com>
2025-05-13 20:46:56 -0300

    create brand e configuração

--- src/routes/brand.routes.ts
+++ src/routes/brand.routes.ts
@@ -1,16 +1,28 @@
-import express from "express";
+import express, { NextFunction } from "express";
 import { Request, Response } from "express";
 import { create, listAll } from "../controllers/brand.controller";
 
 
 const router = express.Router();
 
+const logger = (req: Request, res: Response, next: NextFunction) => {
+    console.log("LOGGED");
+    next();
+};
+
+const createBrandMiddleware = ( req: Request, res: Response, next: NextFunction) => {
+    console.log("descrição: "+req.body.description);
+    next();
+}
+
+router.use(logger);
+
 router.get("/", async (req: Request, res: Response) => {
     const brands = await listAll();
     res.status(200).json(brands);
 });
 
-router.post("/", async (req: Request, res: Response) => {
+router.post("/", createBrandMiddleware, async (req: Request, res: Response) => {
     const { description } = req.body;
 
     const brand = await create(description);
