78d3becb451f0458fe9a2b3d7db042d5e45e0ffd
Isabel Maito <maitoisabel@gmail.com>
2025-05-20 19:59:43 -0300

    Open and configuration AuthorizeMiddleware

---
+++ src/middlewares/authorize.middleware.ts
@@ -0,0 +1,20 @@
+import jwt from "jsonwebtoken"
+import { NextFunction, Request, Response } from "express";
+
+export const AuthorizeMiddleware = (
+    req: Request, 
+    rest: Response, 
+    next: NextFunction
+) => {
+    const { authorization } = req.headers;
+    const secret = process.env.AUTH_SECRET || "";
+
+    jwt.verify(authorization || "", secret, (err) => {
+        if (err) {
+            return rest.status(401).json({ message: "Invalid token" });
+        }
+        next();
+    });
+    
+    console.log(">> ", authorization);
+};
